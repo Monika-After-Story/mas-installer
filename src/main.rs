e696f049a35677c73c94d0c990bd016c4a2bde39
Totally a booplicate <53382877+Booplicate@users.noreply.github.com>
2022-07-27 08:38:41 +0300

    commit remaining bits

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,9 @@
 // #![allow(dead_code, unused_imports, unused_mut, unused_variables)]// TODO: remove me
 mod app_styles;
 mod builder;
+mod errors;
 mod utils;
 
-
-// Include the icon
-#[cfg(feature="compile_icon")]
-static APP_ICON_DATA: &'static [u8] = include_bytes!("static/icon.png");
-#[cfg(not(feature="compile_icon"))]
-static APP_ICON_DATA: &'static [u8] = b"";
-// Include license
-#[cfg(feature="compile_license")]
-static APP_LICENSE: &'static str = include_str!("static/license.md");
-#[cfg(not(feature="compile_license"))]
-static APP_LICENSE: &'static str = "Hello, World!";
-
-
-// use std::path::PathBuf;
 
 use fltk::{
     app::{
@@ -40,13 +27,34 @@ use fltk::{
 };
 
 
+// Include the icon
+#[cfg(feature="compile_icon")]
+static APP_ICON_DATA: &'static [u8] = include_bytes!("static/icon.png");
+#[cfg(not(feature="compile_icon"))]
+static APP_ICON_DATA: &'static [u8] = b"";
+// Include license
+#[cfg(feature="compile_license")]
+static APP_LICENSE: &'static str = include_str!("static/license.md");
+#[cfg(not(feature="compile_license"))]
+static APP_LICENSE: &'static str = "Hello, World!";
+// GH link parts to accept the API
+static ORG_NAME: &'static str = "Monika-After-Story";
+static REPO_NAME: &'static str = "MonikaModDev";
+// IDs of assets in github release
+const DEF_VERSION_ASSET_ID: usize = 1;
+const DLX_VERSION_ASSET_ID: usize = 0;
+
+
 #[derive(Clone, Copy)]
 pub enum Message {
     Close,
     NextPage,
     PrevPage,
     SelectDir,
-    InstallSpritepacks
+    InstallSpritepacksCheck,
+    Downloading,
+    Extracting,
+    Done
 }
 
 
@@ -101,9 +109,18 @@ fn main() {
                     extraction_dir = utils::run_select_dir_dlg(app_styles::SEL_DIR_DLG_PROMPT);
                     path_txt_buf.set_text(extraction_dir.to_str().unwrap_or_default());
                 }
-                Message::InstallSpritepacks => {
+                Message::InstallSpritepacksCheck => {
                     is_deluxe_version = !is_deluxe_version;
-                    println!("is deluxe: {:?}", is_deluxe_version);
+                    // println!("is deluxe: {:?}", is_deluxe_version);
+                }
+                Message::Downloading => {
+                    println!("Downloading");
+                }
+                Message::Extracting => {
+                    println!("Extracting");
+                }
+                Message::Done => {
+                    println!("Done");
                 }
             }
         }
